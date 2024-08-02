e8f1d711fa820409447e74c4a73e093a718bbff8
SKishchenko <userg@yandex.ru>
2024-08-02 11:43:26 +0300

    Change class Employee for private fields

--- src/main/java/ru/netology/Employee.java
+++ src/main/java/ru/netology/Employee.java
@@ -1,11 +1,12 @@
 package ru.netology;
 
 public class Employee {
-    public long id;
+    private long id;
     public String firstName;
-    public String lastName;
-    public String country;
-    public int age;
+    private String lastName;
+    private String country;
+    private int age;
+
 
     public Employee() {
         // Пустой конструктор
@@ -30,4 +31,43 @@ public class Employee {
                 '}';
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
